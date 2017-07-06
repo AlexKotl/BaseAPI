b0aa16c127c87cc394438928427f3ce576c20e8c
Alexander Kotliarov <slicer256@gmail.com>
2017-07-07 00:18:57 +0300

    added images array to base details

--- modules/bases-details.php
+++ modules/bases-details.php
@@ -4,6 +4,19 @@
 	
 	$row = $db->get_row("select * from bases where id = '{$id}'");
 	
+	// filter description
+	$row['description'] = str_replace("\n", "<br>", $row['description']);
+	
+	// list images
+	$images = array();
+	
+	for ($i=1; $i<=5; $i++) {
+		$cur_img = \BaseAPI\Configs\IMAGES_DIR . "/{$row['id']}_{$i}.jpg";
+		if (file_exists($cur_img)) {
+			$images[] = \BaseAPI\Configs\RESOURCES_HOST . "/upload/images/{$row['id']}_{$i}.jpg";
+		}
+	}
+	
 	$data = array(		
 		'id' => $row['id'],
 		'title' => $row['name'],
@@ -14,6 +27,7 @@
 		'price' => $row['price'] ? $row['price'] . ' грн' : '',
 		'posX' => $row['pos_x'],
 		'posY' => $row['pos_y'],
+		'images' => $images
 	);
 		
 ?>
