cb7aacdc0c410de858041e4e49a6970e2939b931
Alexander Kotliarov <slicer256@gmail.com>
2017-07-07 00:04:11 +0300

    bases-list query fix

--- modules/bases-list.php
+++ modules/bases-list.php
@@ -1,12 +1,13 @@
 <?php
 	
-	$res = $db->query("select *, count(comments.id) as comments_count from bases 
+	$res = $db->query("select bases.*, count(comments.id) as comments_count from bases 
 		inner join comments on comments.base_id = bases.id
 		where flag='ok' 
 		group by bases.id
 		order by bases.vip desc, bases.vip_end_date desc, bases.id desc ");
 		
 	while ($row=$db->fetch($res)) {
+		
 		$data[] = array(
 			'id' => $row['id'],
 			'title' => $row['name'],
@@ -17,6 +18,7 @@
 			'rating' => $row['rating'],
 			'price' => $row['price'] ? $row['price'] . ' грн' : '',
 		);
+		
 	}
 	
 ?>
