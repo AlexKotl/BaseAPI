ca532d31d2f821464e8a730b93c65505e5301463
Alexander Kotliarov <slicer256@gmail.com>
2017-07-07 16:43:07 +0300

    dont use var in constant

--- configs/config.default.php
+++ configs/config.default.php
@@ -2,7 +2,7 @@
 	
 	namespace BaseAPI\Configs;
 	
-	const IMAGES_DIR = __DIR__ . '/../../www/upload/images';
+	const IMAGES_DIR = '/domains/base/www/upload/images';
 	const RESOURCES_HOST = 'http://base.kiev.ua';
 	
 ?>
